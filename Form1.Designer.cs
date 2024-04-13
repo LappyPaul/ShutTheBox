64ab8f43e83d8aa103697a90e8e898ec18b5db24
Paul Leonard <32750234+LappyPaul@users.noreply.github.com>
2024-04-13 19:55:58 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chk1 = new System.Windows.Forms.CheckBox();
+            this.chk2 = new System.Windows.Forms.CheckBox();
+            this.chk3 = new System.Windows.Forms.CheckBox();
+            this.chk4 = new System.Windows.Forms.CheckBox();
+            this.chk5 = new System.Windows.Forms.CheckBox();
+            this.chk6 = new System.Windows.Forms.CheckBox();
+            this.chk7 = new System.Windows.Forms.CheckBox();
+            this.chk8 = new System.Windows.Forms.CheckBox();
+            this.chk9 = new System.Windows.Forms.CheckBox();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.btnRoll = new System.Windows.Forms.Button();
+            this.lblTotalRoll = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chk1
+            // 
+            this.chk1.AutoSize = true;
+            this.chk1.Enabled = false;
+            this.chk1.Location = new System.Drawing.Point(33, 102);
+            this.chk1.Name = "chk1";
+            this.chk1.Size = new System.Drawing.Size(44, 24);
+            this.chk1.TabIndex = 0;
+            this.chk1.Text = "1";
+            this.chk1.UseVisualStyleBackColor = true;
+            // 
+            // chk2
+            // 
+            this.chk2.AutoSize = true;
+            this.chk2.Enabled = false;
+            this.chk2.Location = new System.Drawing.Point(83, 102);
+            this.chk2.Name = "chk2";
+            this.chk2.Size = new System.Drawing.Size(44, 24);
+            this.chk2.TabIndex = 1;
+            this.chk2.Text = "2";
+            this.chk2.UseVisualStyleBackColor = true;
+            // 
+            // chk3
+            // 
+            this.chk3.AutoSize = true;
+            this.chk3.Enabled = false;
+            this.chk3.Location = new System.Drawing.Point(133, 102);
+            this.chk3.Name = "chk3";
+            this.chk3.Size = new System.Drawing.Size(44, 24);
+            this.chk3.TabIndex = 2;
+            this.chk3.Text = "3";
+            this.chk3.UseVisualStyleBackColor = true;
+            // 
+            // chk4
+            // 
+            this.chk4.AutoSize = true;
+            this.chk4.Enabled = false;
+            this.chk4.Location = new System.Drawing.Point(183, 102);
+            this.chk4.Name = "chk4";
+            this.chk4.Size = new System.Drawing.Size(44, 24);
+            this.chk4.TabIndex = 3;
+            this.chk4.Text = "4";
+            this.chk4.UseVisualStyleBackColor = true;
+            // 
+            // chk5
+            // 
+            this.chk5.AutoSize = true;
+            this.chk5.Enabled = false;
+            this.chk5.Location = new System.Drawing.Point(233, 102);
+            this.chk5.Name = "chk5";
+            this.chk5.Size = new System.Drawing.Size(44, 24);
+            this.chk5.TabIndex = 4;
+            this.chk5.Text = "5";
+            this.chk5.UseVisualStyleBackColor = true;
+            // 
+            // chk6
+            // 
+            this.chk6.AutoSize = true;
+            this.chk6.Enabled = false;
+            this.chk6.Location = new System.Drawing.Point(283, 102);
+            this.chk6.Name = "chk6";
+            this.chk6.Size = new System.Drawing.Size(44, 24);
+            this.chk6.TabIndex = 5;
+            this.chk6.Text = "6";
+            this.chk6.UseVisualStyleBackColor = true;
+            // 
+            // chk7
+            // 
+            this.chk7.AutoSize = true;
+            this.chk7.Enabled = false;
+            this.chk7.Location = new System.Drawing.Point(333, 102);
+            this.chk7.Name = "chk7";
+            this.chk7.Size = new System.Drawing.Size(44, 24);
+            this.chk7.TabIndex = 6;
+            this.chk7.Text = "7";
+            this.chk7.UseVisualStyleBackColor = true;
+            // 
+            // chk8
+            // 
+            this.chk8.AutoSize = true;
+            this.chk8.Enabled = false;
+            this.chk8.Location = new System.Drawing.Point(383, 102);
+            this.chk8.Name = "chk8";
+            this.chk8.Size = new System.Drawing.Size(44, 24);
+            this.chk8.TabIndex = 7;
+            this.chk8.Text = "8";
+            this.chk8.UseVisualStyleBackColor = true;
+            // 
+            // chk9
+            // 
+            this.chk9.AutoSize = true;
+            this.chk9.Enabled = false;
+            this.chk9.Location = new System.Drawing.Point(433, 102);
+            this.chk9.Name = "chk9";
+            this.chk9.Size = new System.Drawing.Size(44, 24);
+            this.chk9.TabIndex = 8;
+            this.chk9.Text = "9";
+            this.chk9.UseVisualStyleBackColor = true;
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Enabled = false;
+            this.btnConfirm.Location = new System.Drawing.Point(18, 79);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(108, 36);
+            this.btnConfirm.TabIndex = 12;
+            this.btnConfirm.Text = "Confirm";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // btnRoll
+            // 
+            this.btnRoll.Location = new System.Drawing.Point(18, 37);
+            this.btnRoll.Name = "btnRoll";
+            this.btnRoll.Size = new System.Drawing.Size(108, 36);
+            this.btnRoll.TabIndex = 13;
+            this.btnRoll.Text = "Roll";
+            this.btnRoll.UseVisualStyleBackColor = true;
+            this.btnRoll.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblTotalRoll
+            // 
+            this.lblTotalRoll.AutoSize = true;
+            this.lblTotalRoll.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalRoll.Location = new System.Drawing.Point(26, 34);
+            this.lblTotalRoll.Name = "lblTotalRoll";
+            this.lblTotalRoll.Size = new System.Drawing.Size(194, 37);
+            this.lblTotalRoll.TabIndex = 14;
+            this.lblTotalRoll.Text = "Total rolled: ";
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(18, 163);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(108, 36);
+            this.btnRestart.TabIndex = 15;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnReset);
+            this.groupBox1.Controls.Add(this.btnRoll);
+            this.groupBox1.Controls.Add(this.btnRestart);
+            this.groupBox1.Controls.Add(this.btnConfirm);
+            this.groupBox1.Location = new System.Drawing.Point(539, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(148, 214);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Game Controls";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(18, 121);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(108, 36);
+            this.btnReset.TabIndex = 17;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 261);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblTotalRoll);
+            this.Controls.Add(this.chk9);
+            this.Controls.Add(this.chk8);
+            this.Controls.Add(this.chk7);
+            this.Controls.Add(this.chk6);
+            this.Controls.Add(this.chk5);
+            this.Controls.Add(this.chk4);
+            this.Controls.Add(this.chk3);
+            this.Controls.Add(this.chk2);
+            this.Controls.Add(this.chk1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Shut The Box!";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chk1;
+        private System.Windows.Forms.CheckBox chk2;
+        private System.Windows.Forms.CheckBox chk3;
+        private System.Windows.Forms.CheckBox chk4;
+        private System.Windows.Forms.CheckBox chk5;
+        private System.Windows.Forms.CheckBox chk6;
+        private System.Windows.Forms.CheckBox chk7;
+        private System.Windows.Forms.CheckBox chk8;
+        private System.Windows.Forms.CheckBox chk9;
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.Button btnRoll;
+        private System.Windows.Forms.Label lblTotalRoll;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int totalOfCheckBoxesTicked;
+        int countOfCheckBoxesTicked;
+        int totalDiceRoll;
+        const string scoreText = "Total rolled: ";
+        const string welcomeText = "Welcome to Shut The Box!";
+        int rollCount;
+
+        public Form1()
+        {
+            InitializeComponent();
+            lblTotalRoll.Text = welcomeText;
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            countOfCheckBoxesTicked = 0;
+            totalOfCheckBoxesTicked = 0;
+
+            foreach (var chkBoxesChecked in Controls.OfType<CheckBox>())
+            {
+                if (chkBoxesChecked.Checked && chkBoxesChecked.Enabled)
+                {
+                    countOfCheckBoxesTicked += 1;
+
+                    if (countOfCheckBoxesTicked >= 1 && countOfCheckBoxesTicked < 3)
+                    {
+                        string chkBoxNumberChecked = chkBoxesChecked.Text;
+                        totalOfCheckBoxesTicked +=
+                                    Convert.ToInt32(chkBoxNumberChecked.Substring(0, chkBoxesChecked.Text.Length));
+                        //chkBoxesChecked.Enabled = false;
+                    }
+                }
+            }
+
+            if (countOfCheckBoxesTicked == 0)
+            {
+                MessageBox.Show("No move selected, please retry!", "Shut The Box!");
+                //totalOfCheckBoxesTicked = 0;
+            }
+
+            if (countOfCheckBoxesTicked > 2)
+            {
+                MessageBox.Show("Too many numbers chosen - cheat!", "Shut The Box!");
+                totalOfCheckBoxesTicked = 0;
+            }
+
+            if (totalOfCheckBoxesTicked == totalDiceRoll)
+            {
+                foreach (var chkBoxesChecked in Controls.OfType<CheckBox>())
+                {
+                    if (chkBoxesChecked.Checked)
+                    {
+                        chkBoxesChecked.Enabled = false;
+                    }
+                }
+
+                btnRoll.Enabled = true;
+                btnConfirm.Enabled = false;
+                lblTotalRoll.Text = "Please roll again!";
+            }
+            else
+            {
+                MessageBox.Show($"The dice rolled totals {totalDiceRoll}. " +
+                                $"The selections made total {totalOfCheckBoxesTicked}.", "Shut The Box!");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rollCount += 1;
+
+            if (rollCount == 1)
+            {
+                chk1.Enabled = true;
+                chk2.Enabled = true;
+                chk3.Enabled = true;
+                chk4.Enabled = true;
+                chk5.Enabled = true;
+                chk6.Enabled = true;
+                chk7.Enabled = true;
+                chk8.Enabled = true;
+                chk9.Enabled = true;
+                //chk10.Enabled = true;
+                //chk11.Enabled = true;
+                //chk12.Enabled = true;
+            }
+
+
+            btnConfirm.Enabled = true;
+
+            Random random = new Random();
+
+            int dice1 = random.Next(1, 7);
+            int dice2 = random.Next(1, 7);
+            totalDiceRoll = dice1 + dice2;
+
+            lblTotalRoll.Text = scoreText + totalDiceRoll;
+            btnRoll.Enabled = false;
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            foreach (var chkBoxesChecked in Controls.OfType<CheckBox>())
+            {
+                chkBoxesChecked.Enabled = false;
+                chkBoxesChecked.Checked = false;
+                lblTotalRoll.Text = string.Empty;
+                totalDiceRoll = 0;
+                countOfCheckBoxesTicked = 0;
+                rollCount = 0;
+            }
+
+            btnConfirm.Enabled = false;
+            btnRoll.Enabled = true;
+            lblTotalRoll.Text = welcomeText;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            foreach (var chkBoxesChecked in Controls.OfType<CheckBox>())
+            {
+                if (chkBoxesChecked.Checked && chkBoxesChecked.Enabled)
+                {
+                    //chkBoxesChecked.Enabled = true;
+                    chkBoxesChecked.Checked = false;
+                }
+            }
+        }
+    }
+}
+
